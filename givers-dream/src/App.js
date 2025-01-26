7a34cd014a43dcf070a36d9d3bb9d4c4c3567abb
Frederick  Ishimwe <frederickishimwe@Fredericks-MacBook-Air.local>
2025-01-26 18:10:29 +0200

    landingpage-updates

--- givers-dream/src/App.js
+++ givers-dream/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,16 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to The Givers Dream</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a non-profit organization created by 3 ladies.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/thegiversdream/the-givers-dream?tab=readme-ov-file#the-givers-dream"
           target="_blank"
           rel="noopener noreferrer"
         >
-          The givers dream
+          Learn More
         </a>
       </header>
     </div>
